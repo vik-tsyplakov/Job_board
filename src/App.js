e21874aded5bf34880c0490677b8e20f3f610fc3
vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-11-06 22:07:54 +0300

    fonts__correct

--- src/App.js
+++ src/App.js
@@ -4,15 +4,47 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="font-roboto">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1
-          className="text-3xl font-bold underline text-red-100	
+          className="text-3xl font-proxima font-bold underline text-red-100	
 "
         >
           Hello world!
         </h1>
+        <p className="font-proxima font-bold text-[#3A4562] 	">
+          € 54 000—60 000
+        </p>
+        <p className="font-roboto text-[#3A4562]">
+          At WellStar, we all share common goals. That’s what makes us so
+          successful – and such an integral part of our communities. We want the
+          same things, for our organization, for our patients, and for our
+          colleagues. As the most integrated healthcare provider in Georgia,
+          this means we pride ourselves on investing in the communities that we
+          serve. We continue to provide innovative care models, focused on
+          improving quality and access to healthcare. Wellstar Medical Group, a
+          physician-led multi-specialty group in Atlanta, Georgia, is currently
+          recruiting for a BC/BE cardiothoracic surgeon to join their existing
+          cardiovascular program. This is an opportunity to play a key role on a
+          multidisciplinary team of cardiologists and surgeons. The ideal
+          candidate will have five or more years of experience in cardiac
+          surgery. This candidate should be facile with off-pump
+          revascularization, complex aortic surgery, minimally invasive valve
+          and valve repair, transcatheter valve replacement, surgical atrial
+          fibrillation ablation, ventricular assist device placement, and extra
+          corporeal membrane oxygenation. Wellstar is one of the largest
+          integrated healthcare systems in the Southeast with 11 hospitals in
+          Atlanta metro region. With two open heart programs at Kennestone
+          Regional Medical Center and Atlanta Medical Center, Wellstar cardiac
+          surgeons perform over 1200 cardiac procedures per year. The cardiac
+          service line is the only center in Georgia with the Joint Commission’s
+          Comprehensive Cardiac Center certification. Our physicians enjoy a
+          wide range of benefits, including: Very competitive compensation
+          package with bonuses Medical, Dental, and Vision Insurance
+          Occurrence-based Malpractice Coverage Short-term and Long-term
+          Disability Insurance and life insurance
+        </p>
       </header>
     </div>
   );
