6e2481ece767053f42eb4dbaa62290197ae133c8
vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-11-08 15:12:44 +0300

    container__added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="font-roboto">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App pt-[29px]">
+      <div className="container mx-auto max-w-[1400px]">
+        <p className="font-roboto">Edit src and save to reload.</p>
         <h1
           className="text-3xl font-proxima font-bold underline text-red-100	
 "
         >
           Job Board
         </h1>
-      </header>
+      </div>
     </div>
   );
 }
