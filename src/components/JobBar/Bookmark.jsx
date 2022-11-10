50f62b12e1b231e0ec482b90e59eb0fa1d894627
vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-11-10 18:41:30 +0300

    bookmark-adding-functionality

--- src/components/JobBar/Bookmark.jsx
+++ src/components/JobBar/Bookmark.jsx
@@ -4,9 +4,16 @@ import imgBookmark from "../../img/bookmark.svg";
 export default function Bookmark() {
   return (
     <div className=" w-[32px]">
-      <button>
-        <img className="" src={imgBookmark} alt="Bookmark" />{" "}
-      </button>
+      {/*<button className="bookmark" onClick={myFunc}>
+        <img className="" src={imgBookmark} alt="Bookmark" />
+  </button>*/}
+      <div>
+        <input className="bookmark" type="checkbox" />
+      </div>
     </div>
   );
+}
+
+function myFunc() {
+  alert("Make myFunc the pFunc... omg that was horrible i am so sorry");
 }
