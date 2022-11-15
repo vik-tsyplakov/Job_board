93c7a88535088a39e1a1ef3a7f758f08153a3df9
vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-11-15 22:35:37 +0300

    react-router-dom-installed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 import JobBar from "./components/JobBar/JobBar";
 
 //import PaginatedItems from "./components/Pagination/PaginatedItems";
 
 function App() {
-  return (
-    <div className="App pt-[29px] h-full w-full">
-      <div className="container mx-auto max-w-[1436px] px-[9px] sm:px-[18px]">
-        <JobBar />
-        <div className="container mx-auto" id="container">
-          {/*<PaginatedItems />*/}
+  // Connection to API
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App pt-[29px] h-full w-full">
+        <div className="container mx-auto max-w-[1436px] px-[9px] sm:px-[18px]">
+          <ul>
+            {items.map((item) => (
+              <li key={item.id} name={item.name}>
+                <JobBar name={item.name} title={item.title} />
+              </li>
+            ))}
+          </ul>
+          {/*<div className="container mx-auto" id="container">
+            
+            <PaginatedItems />
+            </div>*/}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/JobBar/JobBar.jsx
+++ src/components/JobBar/JobBar.jsx
@@ -1,55 +1,18 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
 import Content from "./Content";
 import RatingDayBookmark from "./RatingDayBookmark";
 
-function JobBar() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    for (let i = 0; i < items.length; i++) {
-      return (
-        <ul>
-          {items.map((item) => (
-            <li key={item.id} name={item.name}>
-              <div className="block smmd:flex max-w-[1400px] sm:h-[164px] h-[206px] sm:bg-[#FFFFFF] bg-[#EFF0F5] rounded-lg px-[16px] py-[24px] mb-[8px] items-center sm:shadow-barlarge shadow-barsmall justify-between">
-                <div className="">
-                  <Content name={item.name} title={item.title} />
-                </div>
-                <div className="smmd:block hidden">
-                  <RatingDayBookmark />
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      );
-    }
-  }
+function JobBar(props) {
+  return (
+    <div className="block smmd:flex max-w-[1400px] sm:h-[164px] h-[206px] sm:bg-[#FFFFFF] bg-[#EFF0F5] rounded-lg px-[16px] py-[24px] mb-[8px] items-center sm:shadow-barlarge shadow-barsmall justify-between">
+      <div className="">
+        <Content name={props.name} title={props.title} />
+      </div>
+      <div className="smmd:block hidden">
+        <RatingDayBookmark />
+      </div>
+    </div>
+  );
 }
 
 export default JobBar;
