1bb9f020dbd1d6e055f05396bf6f2d6139cc29b5
vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-11-15 18:36:43 +0300

    rewrite-api-answer-as-functiontal

--- src/components/JobBar/JobBar.jsx
+++ src/components/JobBar/JobBar.jsx
@@ -1,68 +1,53 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import Content from "./Content";
 import RatingDayBookmark from "./RatingDayBookmark";
-let data = [];
 
-class JobBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: [],
-      title: null,
-      name: null,
-    };
-  }
-  componentDidMount() {
+function JobBar() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
     fetch(
       "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
     )
       .then((res) => res.json())
       .then(
         (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result,
-          });
+          setIsLoaded(true);
+          setItems(result);
         },
         (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
+          setIsLoaded(true);
+          setError(error);
         }
       );
-  }
+  }, []);
 
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      for (let i = 0; i < items.length; i++) {
-        data.push(items[i]);
-        for (let j = 0; j < items.length; j++) {
-          return (
-            <ul>
-              {items.map((item) => (
-                <li key={item.id} name={item.name}>
-                  <div className="block smmd:flex max-w-[1400px] sm:h-[164px] h-[206px] sm:bg-[#FFFFFF] bg-[#EFF0F5] rounded-lg px-[16px] py-[24px] mb-[8px] items-center sm:shadow-barlarge shadow-barsmall justify-between">
-                    <div className="">
-                      <Content name={item.name} title={item.title} />
-                    </div>
-                    <div className="smmd:block hidden">
-                      <RatingDayBookmark />
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          );
-        }
-      }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    for (let i = 0; i < items.length; i++) {
+      return (
+        <ul>
+          {items.map((item) => (
+            <li key={item.id} name={item.name}>
+              <div className="block smmd:flex max-w-[1400px] sm:h-[164px] h-[206px] sm:bg-[#FFFFFF] bg-[#EFF0F5] rounded-lg px-[16px] py-[24px] mb-[8px] items-center sm:shadow-barlarge shadow-barsmall justify-between">
+                <div className="">
+                  <Content name={item.name} title={item.title} />
+                </div>
+                <div className="smmd:block hidden">
+                  <RatingDayBookmark />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      );
     }
   }
 }
