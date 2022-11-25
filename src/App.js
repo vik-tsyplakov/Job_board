88f0cf6fd662f3705b355f98712077ee8e293153
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-11-25 21:32:09 +0400

    app__structure_changed

--- src/App.js
+++ src/App.js
@@ -1,63 +1,27 @@
 import "./App.css";
-import { useState } from "react";
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import JobBar from "./components/JobBar/JobBar";
+import JobBarMainPage from "./components/JobBar/JobBarMainPage";
 import JobDetail from "./components/JobDetail/JobDetail";
 
 //import PaginatedItems from "./components/Pagination/PaginatedItems";
 
 function App() {
-  // Connection to API
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <>
-        <div className="App app-wrapper pt-[29px] h-full w-full">
-          <div className="container mx-auto max-w-[1436px] px-[9px] sm:px-[18px]">
-            <ul>
-              {items.map((item) => (
-                <li key={item.id} name={item.name}>
-                  <JobBar name={item.name} title={item.title} />
-                </li>
-              ))}
-            </ul>
-            {/*<div className="container mx-auto" id="container">
+  return (
+    <>
+      <div className="App app-wrapper pt-[29px] h-full w-full">
+        <div className="container mx-auto max-w-[1436px] px-[9px] sm:px-[18px]">
+          {/*<div className="container mx-auto" id="container">
             <PaginatedItems />
             </div>*/}
 
-            <Routes>
-              <Route path="detail" element={<JobDetail />} />
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<JobBarMainPage />} />
+            <Route path="detail" element={<JobDetail />} />
+          </Routes>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/JobBar/JobBarMainPage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import JobBar from "./JobBar";
+
+export default function JobBarMainPage() {
+  // Connection to API
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map((item) => (
+          <li key={item.id} name={item.name}>
+            <JobBar name={item.name} title={item.title} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
