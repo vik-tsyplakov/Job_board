30f44ae86096b0c211ce2b7b29dd86c58e73f912
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-11-28 19:55:32 +0400

    JobDetailContact-created

--- src/components/JobDetail/JobDetail.jsx
+++ src/components/JobDetail/JobDetail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import "../src/../../JobDetail.css";
+import JobDetailContact from "./JobDetailContact";
 import JobDetailMain from "./JobDetailMain";
 
 export default function JobDetail(props) {
@@ -40,21 +41,7 @@ export default function JobDetail(props) {
               <li key={item.id} name={item.name} img={item.pictures[0]}>
                 <div className="container block lg:flex max-w-[1377px] h-full px-[15px] pt-[24px] pb-[37px] justify-between  ">
                   <JobDetailMain />
-                  <div className="jobDetail__contact flex justify-center max-w-[372px] sm:mx-auto">
-                    <div className="h-[436px] bg-[#2A3047] rounded-lg mt-[21px] lg:mt-[0px] ">
-                      <p>
-                        Department name. University Hospital Giessen.{" "}
-                        {item.name}
-                      </p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Error labore ipsum quos repudiandae nihil repellat
-                      pariatur id sit eius corporis reiciendis odio vel magnam
-                      voluptatibus doloremque, temporibus veniam nesciunt
-                      voluptates fugiat porro? Aperiam reiciendis molestiae enim
-                      harum est ratione maxime quis, perferendis explicabo aut
-                      tenetur error, eius accusamus temporibus repudiandae?
-                    </div>
-                  </div>
+                  <JobDetailContact name={item.name} />
                 </div>
               </li>
             ))}
---
+++ src/components/JobDetail/JobDetailContact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function JobDetailContact(props) {
+  return (
+    <div className=" flex justify-center max-w-[372px] sm:mx-auto">
+      <div className="h-[436px] bg-[#2A3047] rounded-lg mt-[21px] lg:mt-[0px] ">
+        <p>Department name. University Hospital Giessen. {props.name}</p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error labore
+        ipsum quos repudiandae nihil repellat pariatur id sit eius corporis
+        reiciendis odio vel magnam voluptatibus doloremque, temporibus veniam
+        nesciunt voluptates fugiat porro? Aperiam reiciendis molestiae enim
+        harum est ratione maxime quis, perferendis explicabo aut tenetur error,
+        eius accusamus temporibus repudiandae?
+      </div>
+    </div>
+  );
+}
