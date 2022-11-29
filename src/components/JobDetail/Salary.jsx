0c8751de7b17b1d6229c9f5b2cb2fcc37dd75cab
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-11-29 23:16:05 +0400

    salary-right-side-added

--- src/components/JobDetail/Salary.jsx
+++ src/components/JobDetail/Salary.jsx
@@ -6,7 +6,9 @@ export default function Salary(props) {
       <p className="sm:mt-[0px] mt-[4px] font-proxima text-[#3A4562] tracking-[-0.625px] leading-[25px] font-bold text-[20px]">
         € {props.salary}
       </p>
-      <p className="brutto">Brutto, per year</p>
+      <p className="brutto text-[#3A4562] tracking-[-0.5625px] leading-[24px] text-[18px] font-roboto">
+        Brutto, per year
+      </p>
     </div>
   );
 }
