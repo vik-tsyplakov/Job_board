120913a2ef2148f2f4a104eb7e0e1b0752a4a7dd
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-01 20:24:23 +0400

    titile-additional-info-added

---
+++ src/components/JobDetail/Block4.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Block4() {
+  return (
+    <div>
+      <h2 className="text-[#3A4562] font-bold font-proxima text-[28px] tracking-[0.413333px] leading-[34px] pl-[6px] smmd:pl-[0px]">
+        Additional info
+      </h2>
+    </div>
+  );
+}
--- src/components/JobDetail/JobDetailMain.jsx
+++ src/components/JobDetail/JobDetailMain.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Block1 from "./Block1";
 import Block2 from "./Block2";
 import Block3 from "./Block3";
+import Block4 from "./Block4";
 
 export default function JobDetailMain(props) {
   return (
@@ -16,6 +17,9 @@ export default function JobDetailMain(props) {
       </section>
       <section className="block3 sm:px-[10px] px-[10px]">
         <Block3 />
+      </section>
+      <section className="block4 px-[10px] mt-[84px] ">
+        <Block4 />
       </section>
     </div>
   );
