a2d2297bf88ccac7c35765e1f84ba9e672359e0b
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-16 16:20:30 +0400

    createBrowserRouter-started

--- src/components/JobBar/JobBarMainPage.jsx
+++ src/components/JobBar/JobBarMainPage.jsx
@@ -3,11 +3,6 @@ import { useState } from "react";
 import { useEffect } from "react";
 import JobBar from "./JobBar";
 
-import ReactPaginate from "https://cdn.skypack.dev/react-paginate@7.1.3";
-import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
-
-const items = [...Array(20).keys()];
-
 export default function JobBarMainPage() {
   // Connection to API
   const [error, setError] = useState(null);
@@ -31,19 +26,6 @@ export default function JobBarMainPage() {
       );
   }, []);
 
-  function Items({ currentItems }) {
-    return (
-      <div className="items">
-        {currentItems &&
-          currentItems.map((item) => (
-            <div>
-              <h3>Item #{item}</h3>
-            </div>
-          ))}
-      </div>
-    );
-  }
-
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -51,10 +33,7 @@ export default function JobBarMainPage() {
   } else {
     return (
       <div className="App app-wrapper pt-[29px] h-full w-full">
-        <div
-          id="cont"
-          className="container mx-auto max-w-[1436px] px-[9px] sm:px-[18px]"
-        >
+        <div className="container mx-auto max-w-[1436px] px-[9px] sm:px-[18px]">
           <ul>
             {items.map((item) => (
               <li key={item.id} name={item.name} img={item.pictures[0]}>
---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import JobBarMainPage from "./components/JobBar/JobBarMainPage";
+import JobDetail from "./components/JobDetail/JobDetail";
+
+export const Router = createBrowserRouter([]);
